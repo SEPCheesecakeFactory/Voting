d2acdca3699799ef6f848314ab9f8f65075f7afe
Gromykko <alswatchs@yahoo.com>
2025-05-08 12:46:28 +0200

    Update Poll.java
    poll update, private/open and user or usergroups moethods

--- VotingSystemModule/src/Common/Poll.java
+++ VotingSystemModule/src/Common/Poll.java
@@ -1,6 +1,8 @@
 package Common;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 // This could easily be a record class if we embrace immutability (retrieved from the server anyway, so... why mutable?)
 public class Poll implements Serializable
@@ -10,18 +12,21 @@ public class Poll implements Serializable
   private int id;
   private Question[] questions;
   private boolean isClosed;
+  private boolean isPrivate;
 
-  public Poll(String title, String description, int id, Question[] questions, boolean isClosed)
+  private List<Profile> allowedUsers;
+  private List<UserGroup> allowedGroups;
+
+  public Poll(String title, String description, int id, Question[] questions,
+      boolean isClosed)
   {
     this.title = title;
     this.description = description;
     this.id = id;
     this.questions = questions;
     this.isClosed = isClosed;
-  }
-
-  public Poll(String title, String description, int id, Question[] questions) {
-    this(title, description, id, questions, false); // default to not closed
+    this.allowedUsers = new ArrayList<>();
+    this.allowedGroups = new ArrayList<>();
   }
 
   public String getTitle()
@@ -64,14 +69,89 @@ public class Poll implements Serializable
     this.questions = questions;
   }
 
-  public boolean isClosed() {
+  public boolean isClosed()
+  {
     return isClosed;
   }
-  public void setClosed(boolean closed) {
-    isClosed = closed;
-  }
 
-  public void closePoll() {
+  public void closePoll()
+  {
     this.isClosed = true;
   }
+
+  public boolean isPrivate()
+  {
+    return isPrivate;
+  }
+
+  public List<Profile> getAllowedUsers()
+  {
+    return allowedUsers;
+  }
+
+  public List<UserGroup> getAllowedGroups()
+  {
+    return allowedGroups;
+  }
+
+  public void addAllowedUser(Profile profile)
+  {
+    for (Profile p : allowedUsers)
+    {
+      if (p.getId() == profile.getId())
+      {
+        return;
+      }
+    }
+    allowedUsers.add(profile);
+  }
+
+  public void removeAllowedUser(Profile profile)
+  {
+    allowedUsers.remove(profile);
+  }
+
+  public void addAllowedGroup(UserGroup group)
+  {
+    for (UserGroup g : allowedGroups)
+    {
+      if (g.equals(group))
+      {
+        return;
+      }
+    }
+    allowedGroups.add(group);
+  }
+
+  public void removeAllowedGroup(UserGroup group)
+  {
+    allowedGroups.remove(group);
+  }
+
+  public boolean canVote(Profile user)
+  {
+    if (!isPrivate)
+    {
+      return true;
+    }
+    for (Profile allowed : allowedUsers)
+    {
+      if (allowed.getId() == user.getId())
+      {
+        return true;
+      }
+    }
+    for (UserGroup group : allowedGroups)
+    {
+      for (Profile member : group.getMembers())
+      {
+        if (member.getId() == user.getId())
+        {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
+

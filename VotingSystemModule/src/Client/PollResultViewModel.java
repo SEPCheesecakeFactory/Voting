f10d9998be91145b071863c2fc899aac4f090c7d
Piotr Junosz <piotrju2005@wp.pl>
2025-05-08 12:08:17 +0200

    [modify] PollResult MVVM

--- VotingSystemModule/src/Client/PollResultViewModel.java
+++ VotingSystemModule/src/Client/PollResultViewModel.java
@@ -4,43 +4,42 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
-public class PollResultViewModel implements PropertyChangeListener, PropertyChangeSubject
-{
-  private PollResultRequestService pollResultRequestService;
+public class PollResultViewModel implements PropertyChangeListener, PropertyChangeSubject {
+  private final PollResultRequestService pollResultRequestService;
   private final PropertyChangeSupport support;
-  public PollResultViewModel(PollResultRequestService pollResultRequestService)
-  {
-    this.pollResultRequestService=pollResultRequestService;
+
+  public PollResultViewModel(PollResultRequestService pollResultRequestService) {
+    this.pollResultRequestService = pollResultRequestService;
     support = new PropertyChangeSupport(this);
-    this.pollResultRequestService.addPropertyChangeListener("PollResult",this);
+    this.pollResultRequestService.addPropertyChangeListener("PollResult", this);
   }
 
-  @Override public void addPropertyChangeListener(
-      PropertyChangeListener listener)
-  {
-
+  public void requestPollResult(int pollID) {
+    pollResultRequestService.sendResultRequest(pollID);
   }
 
-  @Override public void addPropertyChangeListener(String name,
-      PropertyChangeListener listener)
-  {
-
+  @Override
+  public void addPropertyChangeListener(PropertyChangeListener listener) {
+    support.addPropertyChangeListener(listener);
   }
 
-  @Override public void removePropertyChangeListener(
-      PropertyChangeListener listener)
-  {
-
+  @Override
+  public void addPropertyChangeListener(String name, PropertyChangeListener listener) {
+    support.addPropertyChangeListener(name, listener);
   }
 
-  @Override public void removePropertyChangeListener(String name,
-      PropertyChangeListener listener)
-  {
-
+  @Override
+  public void removePropertyChangeListener(PropertyChangeListener listener) {
+    support.removePropertyChangeListener(listener);
   }
 
-  @Override public void propertyChange(PropertyChangeEvent evt)
-  {
+  @Override
+  public void removePropertyChangeListener(String name, PropertyChangeListener listener) {
+    support.removePropertyChangeListener(name, listener);
+  }
 
+  @Override
+  public void propertyChange(PropertyChangeEvent evt) {
+    support.firePropertyChange(evt);
   }
 }
--- VotingSystemModule/src/Common/PollResult.java
+++ VotingSystemModule/src/Common/PollResult.java
@@ -5,8 +5,24 @@ import java.util.Map;
 public class PollResult
 {
   private Map<Integer, Integer> choiceVoters;
+  private Poll poll;
+  public PollResult(Poll poll, Map<Integer, Integer> choiceVoters)
+  {
+    this.choiceVoters = choiceVoters;
+    this.poll=poll;
+  }
   public int getNumberOfVoters(int choiceID)
   {
     return choiceVoters.get(choiceID);
+  }
+
+  public Map<Integer, Integer> getChoiceVoters()
+  {
+    return choiceVoters;
+  }
+
+  public Poll getPoll()
+  {
+    return poll;
   }
 }

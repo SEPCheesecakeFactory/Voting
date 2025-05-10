49ad3a2853a676d5254759b3f45336bbcd5e14a9
Eduard Fekete <edko.fek@gmail.com>
2025-05-10 10:53:24 +0200

    [Fix] Added getter/setter for message type

--- VotingSystemModule/src/Common/Message.java
+++ VotingSystemModule/src/Common/Message.java
@@ -9,24 +9,43 @@ import java.util.ArrayList;
 
 public class Message
 {
-  public enum MessageType { Response, SendVote, RequestPollResult };
+  public enum MessageType
+  {Response, SendVote, RequestPollResult}
+
+  ;
   private MessageType type;
   private JsonObject params;
   private static final Gson G = new Gson();
+
   public Message(MessageType type)
   {
     this.type = type;
     this.params = new JsonObject();
   }
-  public <T> T getParam(String name, Type typeOfT) {
+
+  public <T> T getParam(String name, Type typeOfT)
+  {
     JsonElement elem = params.get(name);
     return new Gson().fromJson(elem, typeOfT);
   }
-  public <T> void addParam(String name, T object) {
+
+  public <T> void addParam(String name, T object)
+  {
     params.add(name, G.toJsonTree(object));
   }
+
   @Override public String toString()
   {
     return type.name();
+  }
+
+  public MessageType getType()
+  {
+    return type;
+  }
+
+  public void setType(MessageType type)
+  {
+    this.type = type;
   }
 }

a900c88eef9267d77ffa8b372272505bb90b82a9
Gromykko <alswatchs@yahoo.com>
2025-05-14 15:33:17 +0200

    [upd] comment

--- VotingSystemModule/src/Client/Login/LoginViewController.java
+++ VotingSystemModule/src/Client/Login/LoginViewController.java
@@ -23,7 +23,7 @@ public class LoginViewController {
 
   public void init(LoginViewModel viewModel) {
     this.viewModel = viewModel;
-    // Optional: bind or listen to model changes if needed
+    // listen to model if changes needed maybe
   }
 
   @FXML

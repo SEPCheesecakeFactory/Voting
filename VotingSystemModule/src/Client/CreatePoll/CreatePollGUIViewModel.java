b13376afa7af76e15b25cb048e2148a4335df136
Gromykko <alswatchs@yahoo.com>
2025-05-15 17:34:11 +0200

    [new] createpollguiview

---
+++ VotingSystemModule/src/Client/CreatePoll/CreatePollGUIViewModel.java
@@ -0,0 +1,79 @@
+package Client.CreatePoll;
+
+import Common.Poll;
+
+import java.util.List;
+
+public class CreatePollGUIViewModel
+{
+
+  private String pollTitle = "";
+  private String pollDescription = "";
+  private List<CreatePollViewModel.Question> questions;
+  private CreatePollService service;
+  private boolean isPrivate = false;
+
+  public boolean isPrivate()
+  {
+    return isPrivate;
+  }
+
+  public void togglePrivacy()
+  {
+    isPrivate = !isPrivate;
+  }
+
+  // fake
+  public CreatePollGUIViewModel()
+  {
+    this.service = new CreatePollService()
+    {
+      public void createPoll(Poll poll)
+      {
+        System.out.println("Poll created: " + poll.getTitle());
+      }
+    };
+  }
+
+  public void setPollTitle(String title)
+  {
+    this.pollTitle = title;
+  }
+
+  public void setPollDescription(String description)
+  {
+    this.pollDescription = description;
+  }
+
+  public void setQuestions(List<CreatePollViewModel.Question> questions)
+  {
+    this.questions = questions;
+  }
+
+  public void createPoll()
+  {
+    if (questions == null || questions.isEmpty())
+    {
+      throw new IllegalStateException("No questions to create poll");
+    }
+
+    Common.Question[] qArray = new Common.Question[questions.size()];
+
+    for (int i = 0; i < questions.size(); i++)
+    {
+      CreatePollViewModel.Question localQ = questions.get(i);
+      var choiceTexts = localQ.getChoices();
+      Common.ChoiceOption[] options = new Common.ChoiceOption[choiceTexts.size()];
+      for (int j = 0; j < choiceTexts.size(); j++)
+      {
+        options[j] = new Common.ChoiceOption(j, choiceTexts.get(j));
+      }
+      qArray[i] = new Common.Question(options, i, localQ.getTitle(), "");
+    }
+
+    Poll poll = new Poll(pollTitle, pollDescription, 0, qArray, false);
+    service.createPoll(poll);
+    poll.setPrivate(isPrivate);
+  }
+
+}

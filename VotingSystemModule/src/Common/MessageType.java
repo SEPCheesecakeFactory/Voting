35efe88445576b89485f3620bbda0542858bf873
Eduard Fekete <edko.fek@gmail.com>
2025-05-22 17:51:11 +0200

    [Fix] Register bug

--- VotingSystemModule/src/Common/MessageType.java
+++ VotingSystemModule/src/Common/MessageType.java
@@ -17,5 +17,19 @@ public enum MessageType
   SendCreatedPoll,
   LookupUser,
   SendLookupUserResult,
-  SendVoteGroup, SendPoll, SendCreateVoteGroupRequest, SendPollAccess, LookupGroup, SendLookupGroupResult, DisplayPollRequest, SendAvailablePolls, GetAvailablePolls, SendUserGroupsRequest, SendUserGroups, MapConnectionFirstSetup, SendLogin, SendRegister, Test
+  SendVoteGroup,
+  SendPoll,
+  SendCreateVoteGroupRequest,
+  SendPollAccess,
+  LookupGroup,
+  SendLookupGroupResult,
+  DisplayPollRequest,
+  SendAvailablePolls,
+  GetAvailablePolls,
+  SendUserGroupsRequest,
+  SendUserGroups,
+  MapConnectionFirstSetup,
+  SendLogin,
+  SendRegister,
+  Test
 }
